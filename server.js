e31b9a631e1033bb93b9cc37817db10db6977d55
baksh97 <kashishbansal4797@gmail.com>
2017-01-03 22:36:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one' , function(req,res){
-   res.send('Article one has been requested.') 
+   res.send('Article one has been requested.') ;
 });
 
 app.get('/ui/style.css', function (req, res) {
