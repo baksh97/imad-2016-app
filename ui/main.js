6316cf5f264c518f3fc964f30e07889519c40e75
baksh97 <kashishbansal4797@gmail.com>
2017-01-04 15:59:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,13 +1,3 @@
 console.log('Loaded!');
 
 //moving image
-
-leftMargin=0;
-var img = document.getElementById('img');
-function moveRight(){
-    leftMargin = leftMargin+10;
-    img.style.marginLeft = leftMargin+'px'
-}
-img.onclick = function(){
-    var interval = setInterval(moveRight,10);
-}
