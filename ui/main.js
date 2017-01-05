6f6d33c0023734496b278d8ed5ea6efdc6bf46fa
baksh97 <kashishbansal4797@gmail.com>
2017-01-05 15:36:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,7 @@
-console.log('Loaded!');
-
-//moving image
+var button = document.getElementById("button");
+button.onClick = function(){
+    counter = 0;
+    counter=counter+1;
+    var span =document.getElementById("count");
+    span.innerHTML=counter.toString();
+}  
